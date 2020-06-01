22923e4bbdf37c737cba55db815fe1eac1943da6
Ahmed EL-Dakhakhny <>
2020-06-01 15:06:07 +0200

    adding cars recycler view adapter backbone

---
+++ app/src/main/java/com/ahmed/eldakhakhny/softexpert/task/CarsAdapter.java
@@ -0,0 +1,34 @@
+package com.ahmed.eldakhakhny.softexpert.task;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CarsAdapter extends RecyclerView.Adapter<CarsAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        //TODO
+    }
+
+    @Override
+    public int getItemCount() {
+        //TODO
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            //TODO
+        }
+    }
+}
