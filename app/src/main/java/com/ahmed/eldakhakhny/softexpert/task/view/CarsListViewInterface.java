87b7e7c65bbb5ff28b84200a663843f60ad66518
Ahmed EL-Dakhakhny <>
2020-06-01 15:05:11 +0200

    adding view interface for cars to be used in displaying response from API

---
+++ app/src/main/java/com/ahmed/eldakhakhny/softexpert/task/view/CarsListViewInterface.java
@@ -0,0 +1,12 @@
+package com.ahmed.eldakhakhny.softexpert.task.view;
+
+import com.ahmed.eldakhakhny.softexpert.task.model.Car;
+
+import java.util.ArrayList;
+
+public interface CarsListViewInterface {
+    void onGetCarsResponse(ArrayList<Car> cars);
+    void onError(String errorCode,String message);
+    void onFailure(String message);
+
+}
